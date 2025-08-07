93d7a2c67e332ed3c92ba8de26e48ddad6b73fa1
Nick Hammond <nick@nickhammond.com>
2025-08-07 13:01:38 -0700

    Update Honeybadger.notify signature to match new keyword style

--- spec/uniform_notifier/honeybadger_spec.rb
+++ spec/uniform_notifier/honeybadger_spec.rb
@@ -12,14 +12,14 @@ RSpec.describe UniformNotifier::HoneybadgerNotifier do
   end
 
   it 'should notify honeybadger' do
-    expect(Honeybadger).to receive(:notify).with(UniformNotifier::Exception.new('notify honeybadger'), {})
+    expect(Honeybadger).to receive(:notify).with(UniformNotifier::Exception.new('notify honeybadger'))
 
     UniformNotifier.honeybadger = true
     UniformNotifier::HoneybadgerNotifier.out_of_channel_notify(title: 'notify honeybadger')
   end
 
   it 'should notify honeybadger' do
-    expect(Honeybadger).to receive(:notify).with(UniformNotifier::Exception.new('notify honeybadger'), { foo: :bar })
+    expect(Honeybadger).to receive(:notify).with(UniformNotifier::Exception.new('notify honeybadger'), foo: :bar)
 
     UniformNotifier.honeybadger = { foo: :bar }
     UniformNotifier::HoneybadgerNotifier.out_of_channel_notify('notify honeybadger')
